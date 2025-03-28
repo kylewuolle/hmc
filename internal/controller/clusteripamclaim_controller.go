03dffb323e163cd7ea673892039322efd14ae25a
Kyle Wuolle <kyle.wuolle@gmail.com>
2025-03-28 12:36:56 -0700

    Fixed issues per review: - Created cluster ipam controller to reconcile ClusterIPAM object - Added test for cluster ipam controller - various other fixes

--- internal/controller/clusteripamclaim_controller.go
+++ internal/controller/clusteripamclaim_controller.go
@@ -26,28 +26,21 @@ import (
 	ipamv1 "sigs.k8s.io/cluster-api/exp/ipam/api/v1beta1"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
-	"sigs.k8s.io/controller-runtime/pkg/controller/controllerutil"
-	"sigs.k8s.io/controller-runtime/pkg/log"
+	"sigs.k8s.io/controller-runtime/pkg/controller"
 
 	kcm "github.com/K0rdent/kcm/api/v1alpha1"
 	"github.com/K0rdent/kcm/internal/utils"
+	"github.com/K0rdent/kcm/internal/utils/ratelimit"
 )
 
-// ClusterIPAMClaimReconciler reconciles a ClusterIPAMClaim object
 type ClusterIPAMClaimReconciler struct {
 	client.Client
 	Scheme             *runtime.Scheme
 	defaultRequeueTime time.Duration
 }
 
-// +kubebuilder:rbac:groups=k0rdent.mirantis.com.k0rdent.mirantis.com,resources=clusteripamclaims,verbs=get;list;watch;create;update;patch;delete
-// +kubebuilder:rbac:groups=k0rdent.mirantis.com.k0rdent.mirantis.com,resources=clusteripamclaims/status,verbs=get;update;patch
-// +kubebuilder:rbac:groups=k0rdent.mirantis.com.k0rdent.mirantis.com,resources=clusteripamclaims/finalizers,verbs=update
-
-// Reconcile is part of the main kubernetes reconciliation loop which aims to
-// move the current state of the cluster closer to the desired state.
 func (r *ClusterIPAMClaimReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Result, error) {
-	l := log.FromContext(ctx)
+	l := ctrl.LoggerFrom(ctx)
 
 	ci := &kcm.ClusterIPAMClaim{}
 	if err := r.Client.Get(ctx, req.NamespacedName, ci); err != nil {
@@ -65,163 +58,117 @@ func (r *ClusterIPAMClaimReconciler) Reconcile(ctx context.Context, req ctrl.Req
 		Kind:       "ClusterIPAMClaim",
 	}
 
-	if !ci.DeletionTimestamp.IsZero() {
-		l.Info("Deleting ClusterIpamClaim")
-		return r.Delete(ctx, ci)
-	}
-
-	if controllerutil.AddFinalizer(ci, kcm.ClusterIPAMClaimFinalizer) {
-		if err := r.Client.Update(ctx, ci); err != nil {
-			return ctrl.Result{}, fmt.Errorf("failed to update ClusterIPAMClaim %s/%s: %w", ci.Namespace, ci.Name, err)
-		}
-	}
-
-	if err := r.CreateClusterIPAM(ctx, ci); err != nil {
+	if err := r.createOrUpdateClusterIPAM(ctx, ci); err != nil {
 		return ctrl.Result{}, fmt.Errorf("failed to create ClusterIPAM %s/%s: %w", ci.Namespace, ci.Name, err)
 	}
 	return r.updateStatus(ctx, ci)
 }
 
-func (r *ClusterIPAMClaimReconciler) updateStatus(ctx context.Context, ci *kcm.ClusterIPAMClaim) (ctrl.Result, error) {
-	requeue := false
-
-	if ci.Status.ClusterIPAMRef.Name != "" {
-		ipam := kcm.ClusterIPAM{}
-		err := r.Client.Get(ctx, client.ObjectKey{
-			Namespace: ci.Status.ClusterIPAMRef.Namespace,
-			Name:      ci.Status.ClusterIPAMRef.Name,
-		}, &ipam)
-		if err != nil {
-			return ctrl.Result{}, fmt.Errorf("failed to get ClusterIPAM %s/%s: %w", ci.Namespace, ci.Name, err)
-		}
-
-		ipam.Status.Phase = kcm.IpamPhasePending
-		claims := ipam.Spec.NodeIPClaims
-		claims = append(claims, ipam.Spec.ClusterIPClaims...)
-		claims = append(claims, ipam.Spec.ExternalIPClaims...)
-
-		allBound := true
-		for _, claimRef := range claims {
-			ipClaim := ipamv1.IPAddressClaim{}
-
-			err := r.Client.Get(ctx, client.ObjectKey{Name: claimRef.Name, Namespace: claimRef.Namespace}, &ipClaim)
-			if err != nil {
-				return ctrl.Result{}, fmt.Errorf("failed to get ip claim: %s : %w", claimRef.Name, err)
-			}
-
-			if ipClaim.Status.AddressRef.Name == "" {
-				allBound = false
-				requeue = true
-			}
-		}
-
-		if allBound {
-			ipam.Status.Phase = kcm.IpamPhaseBound
-			requeue = false
-		}
-
-		ci.Status.Bound = allBound
-		if err := r.Status().Update(ctx, ci); err != nil {
-			return ctrl.Result{}, fmt.Errorf("failed to update ClusterIPAMClaim status: %w", err)
-		}
-
-		if err := r.Status().Update(ctx, &ipam); err != nil {
-			return ctrl.Result{}, fmt.Errorf("failed to update ClusterIPAM status: %w", err)
-		}
+func (r *ClusterIPAMClaimReconciler) updateStatus(ctx context.Context, clusterIPAMClaim *kcm.ClusterIPAMClaim) (ctrl.Result, error) {
+	clusterIPAM := kcm.ClusterIPAM{}
+	err := r.Client.Get(ctx, client.ObjectKey{
+		Namespace: clusterIPAMClaim.Namespace,
+		Name:      clusterIPAMClaim.Name,
+	}, &clusterIPAM)
+	if err != nil {
+		return ctrl.Result{}, fmt.Errorf("failed to get ClusterIPAM %s/%s: %w", clusterIPAMClaim.Namespace, clusterIPAMClaim.Name, err)
 	}
 
-	if requeue {
+	clusterIPAMClaim.Status.ClusterIPAMRef = corev1.TypedLocalObjectReference{Name: clusterIPAMClaim.Name}
+	clusterIPAMClaim.Status.Bound = clusterIPAM.Status.Phase == kcm.ClusterIpamPhaseBound
+
+	if err := r.Status().Update(ctx, clusterIPAMClaim); err != nil {
+		return ctrl.Result{}, fmt.Errorf("failed to update ClusterIPAMClaim status: %w", err)
+	}
+
+	if !clusterIPAMClaim.Status.Bound {
 		return ctrl.Result{RequeueAfter: r.defaultRequeueTime}, nil
 	}
 
 	return ctrl.Result{}, nil
 }
 
-func (r *ClusterIPAMClaimReconciler) CreateClusterIPAM(ctx context.Context, ci *kcm.ClusterIPAMClaim) (returnErr error) {
-	ipam := kcm.ClusterIPAM{
-		ObjectMeta: metav1.ObjectMeta{Name: ci.Name, Namespace: ci.Namespace},
+func (r *ClusterIPAMClaimReconciler) createOrUpdateClusterIPAM(ctx context.Context, clusterIPAMClaim *kcm.ClusterIPAMClaim) (returnErr error) {
+	clusterIPAM := kcm.ClusterIPAM{
+		ObjectMeta: metav1.ObjectMeta{Name: clusterIPAMClaim.Name, Namespace: clusterIPAMClaim.Namespace},
 		Spec: kcm.ClusterIPAMSpec{
-			Provider: ci.Spec.Provider,
+			Provider: clusterIPAMClaim.Spec.Provider,
 		},
 	}
 
-	ci.Status.ClusterIPAMRef = corev1.ObjectReference{Name: ci.Name, Namespace: ci.Namespace}
-	if err := r.Client.Get(ctx, client.ObjectKey{Name: ci.Name, Namespace: ci.Namespace}, &ipam); err != nil && !apierrors.IsNotFound(err) {
-		return fmt.Errorf("failed to get ClusterIPAM %s/%s: %w", ci.Namespace, ci.Name, err)
+	if err := r.Client.Get(ctx, client.ObjectKeyFromObject(clusterIPAMClaim), &clusterIPAM); client.IgnoreNotFound(err) != nil {
+		return fmt.Errorf("failed to get ClusterIPAM %s: %w", client.ObjectKeyFromObject(clusterIPAMClaim), err)
 	}
 
-	clusterIPAMSpec := ipam.Spec
+	clusterIPAMSpec := clusterIPAM.Spec
 	defer func() {
-		utils.AddOwnerReference(&ipam, ci)
-		_, err := ctrl.CreateOrUpdate(ctx, r.Client, &ipam, func() error {
-			ipam.Spec = clusterIPAMSpec
+		utils.AddOwnerReference(&clusterIPAM, clusterIPAMClaim)
+		_, err := ctrl.CreateOrUpdate(ctx, r.Client, &clusterIPAM, func() error {
+			clusterIPAM.Spec = clusterIPAMSpec
 			return nil
 		})
 		if err != nil {
-			returnErr = fmt.Errorf("failed to create or update ClusterIPAM %s/%s: %w", ci.Namespace, ci.Name, err)
+			returnErr = fmt.Errorf("failed to create or update ClusterIPAM %s/%s: %w", clusterIPAMClaim.Namespace, clusterIPAMClaim.Name, err)
 		}
 	}()
 
-	claims, err := r.createOrUpdateIPClaims(ctx, ci, clusterIPAMSpec.NodeIPClaims, ci.Spec.NodeIPPool)
+	claims, err := r.createOrDeleteIPClaims(ctx, clusterIPAMClaim, clusterIPAMSpec.NodeIPClaims, clusterIPAMClaim.Spec.NodeIPPool)
 	if err != nil {
-		return fmt.Errorf("failed to create or update node ip claims for pool %s: %w", ci.Spec.NodeIPPool.Name, err)
+		return fmt.Errorf("failed to create or update node ip claims for pool %s: %w", clusterIPAMClaim.Spec.NodeIPPool.Name, err)
 	}
 	clusterIPAMSpec.NodeIPClaims = claims
 
-	claims, err = r.createOrUpdateIPClaims(ctx, ci, clusterIPAMSpec.ClusterIPClaims, ci.Spec.ClusterIPPool)
+	claims, err = r.createOrDeleteIPClaims(ctx, clusterIPAMClaim, clusterIPAMSpec.ClusterIPClaims, clusterIPAMClaim.Spec.ClusterIPPool)
 	if err != nil {
-		return fmt.Errorf("failed to create or update cluster ip claims for pool %s: %w", ci.Spec.ClusterIPPool.Name, err)
+		return fmt.Errorf("failed to create or update cluster ip claims for pool %s: %w", clusterIPAMClaim.Spec.ClusterIPPool.Name, err)
 	}
 	clusterIPAMSpec.ClusterIPClaims = claims
 
-	claims, err = r.createOrUpdateIPClaims(ctx, ci, clusterIPAMSpec.ExternalIPClaims, ci.Spec.ExternalIPPool)
+	claims, err = r.createOrDeleteIPClaims(ctx, clusterIPAMClaim, clusterIPAMSpec.ExternalIPClaims, clusterIPAMClaim.Spec.ExternalIPPool)
 	if err != nil {
-		return fmt.Errorf("failed to create or update cluster ip claims for pool %s: %w", ci.Spec.ExternalIPPool.Name, err)
+		return fmt.Errorf("failed to create or update cluster ip claims for pool %s: %w", clusterIPAMClaim.Spec.ExternalIPPool.Name, err)
 	}
 	clusterIPAMSpec.ExternalIPClaims = claims
 	return nil
 }
 
-func (r *ClusterIPAMClaimReconciler) createOrUpdateIPClaims(ctx context.Context, ci *kcm.ClusterIPAMClaim, claims []corev1.ObjectReference, ipPool kcm.IPPoolSpec) ([]corev1.ObjectReference, error) {
+func (r *ClusterIPAMClaimReconciler) createOrDeleteIPClaims(ctx context.Context, ci *kcm.ClusterIPAMClaim, claims []corev1.ObjectReference, ipPool kcm.IPPoolSpec) ([]corev1.ObjectReference, error) {
 	desiredCount := ipPool.Count
 
-	if claims != nil {
-		currentCount := len(claims)
-		if currentCount > desiredCount {
-			// remove from the end of the list
-			removalList := claims[desiredCount:currentCount]
-			claims = claims[0:desiredCount]
+	currentCount := len(claims)
+	if currentCount > desiredCount {
+		// remove from the end of the list
+		removalList := claims[desiredCount:currentCount]
+		claims = claims[0:desiredCount]
 
-			for _, toRemove := range removalList {
-				err := r.Client.Delete(ctx, &ipamv1.IPAddressClaim{ObjectMeta: metav1.ObjectMeta{
-					Name:      toRemove.Name,
-					Namespace: toRemove.Namespace,
-				}})
+		for _, toRemove := range removalList {
+			err := r.Client.Delete(ctx, &ipamv1.IPAddressClaim{ObjectMeta: metav1.ObjectMeta{
+				Name:      toRemove.Name,
+				Namespace: toRemove.Namespace,
+			}})
 
-				if client.IgnoreNotFound(err) != nil {
-					return claims, fmt.Errorf("failed to delete IPAddressClaim: %s/%s", toRemove.Name, toRemove.Namespace)
-				}
+			if client.IgnoreNotFound(err) != nil {
+				return nil, fmt.Errorf("failed to delete IPAddressClaim: %s/%s", toRemove.Name, toRemove.Namespace)
 			}
-			// we've reached the desired count
-			desiredCount = 0
-		} else if currentCount <= desiredCount {
-			desiredCount -= currentCount
 		}
+		// we've reached the desired count
+		desiredCount = 0
+	} else if currentCount <= desiredCount {
+		desiredCount -= currentCount
 	}
 
-	if ipPool.Name != "" {
-		for range desiredCount {
-			claim, err := r.CreateIPAddressClaim(ctx, ci.Namespace, ipPool.TypedLocalObjectReference, ci)
-			if err != nil {
-				return claims, fmt.Errorf("failed to create node ip address claims for: %s pool : %w", ipPool.Name, err)
-			}
-			claims = append(claims, claim)
+	for range desiredCount {
+		claim, err := r.createIPAddressClaim(ctx, ci.Namespace, ipPool.TypedLocalObjectReference, ci)
+		if err != nil {
+			return nil, fmt.Errorf("failed to create node ip address claims for: %s pool : %w", ipPool.Name, err)
 		}
+		claims = append(claims, claim)
 	}
+
 	return claims, nil
 }
 
-func (r *ClusterIPAMClaimReconciler) CreateIPAddressClaim(ctx context.Context, namespace string, pool corev1.TypedLocalObjectReference, ci *kcm.ClusterIPAMClaim) (corev1.ObjectReference, error) {
+func (r *ClusterIPAMClaimReconciler) createIPAddressClaim(ctx context.Context, namespace string, pool corev1.TypedLocalObjectReference, ci *kcm.ClusterIPAMClaim) (corev1.ObjectReference, error) {
 	claim := ipamv1.IPAddressClaim{
 		ObjectMeta: metav1.ObjectMeta{
 			Namespace:    namespace,
@@ -233,30 +180,19 @@ func (r *ClusterIPAMClaimReconciler) CreateIPAddressClaim(ctx context.Context, n
 	}
 
 	utils.AddOwnerReference(&claim, ci)
-	utils.AddLabel(&claim, kcm.OwnedByLabel, ci.Name)
 	if err := r.Create(ctx, &claim); err != nil {
-		return corev1.ObjectReference{Name: claim.Name, Namespace: namespace}, fmt.Errorf("failed to create IP address claim: %w", err)
+		return corev1.ObjectReference{}, fmt.Errorf("failed to create IP address claim: %w", err)
 	}
 	return corev1.ObjectReference{Name: claim.Name, Namespace: namespace}, nil
-}
-
-func (r *ClusterIPAMClaimReconciler) Delete(ctx context.Context, ci *kcm.ClusterIPAMClaim) (ctrl.Result, error) {
-	l := ctrl.LoggerFrom(ctx)
-
-	l.Info("Removing Finalizer", "finalizer", kcm.ClusterIPAMClaimFinalizer)
-	if controllerutil.RemoveFinalizer(ci, kcm.ClusterIPAMClaimFinalizer) {
-		if err := r.Client.Update(ctx, ci); err != nil {
-			return ctrl.Result{}, fmt.Errorf("failed to update ClusterIPAMClaim %s/%s: %w", ci.Namespace, ci.Name, err)
-		}
-	}
-	l.Info("ClusterIPAMClaim deleted")
-	return ctrl.Result{}, nil
 }
 
 // SetupWithManager sets up the controller with the Manager.
 func (r *ClusterIPAMClaimReconciler) SetupWithManager(mgr ctrl.Manager) error {
 	r.defaultRequeueTime = 10 * time.Second
 	return ctrl.NewControllerManagedBy(mgr).
+		WithOptions(controller.TypedOptions[ctrl.Request]{
+			RateLimiter: ratelimit.DefaultFastSlow(),
+		}).
 		For(&kcm.ClusterIPAMClaim{}).
 		Complete(r)
 }
